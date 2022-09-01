6fc8ce8c3c48bfe096151993f141a66c58166a0d
yossef-khaled <yossef.k.y333@gmail.com>
2022-09-01 16:23:42 +0200

    Use users routes

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import express, { Request, Response } from 'express'
 import bodyParser from 'body-parser'
+import usersRoute from './routes/usersRoutes'
 
 const app: express.Application = express()
 const port: string = "3000"
@@ -9,6 +10,9 @@ app.use(bodyParser.json())
 app.get('/', function (req: Request, res: Response) {
     res.send('Hello World!')
 })
+
+// call all the routes
+usersRoute(app);
 
 app.listen(3000, function () {
     console.log(`🚀🚀 Storefont server started on port ${port}...`);
