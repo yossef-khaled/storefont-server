2af4bad128e3d11bb6f36ab3a5ceb84f3e1ce4d1
yossef-khaled <yossef.k.y333@gmail.com>
2022-09-03 22:00:47 +0200

    Use products routes in the index file

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,12 @@
+//Import from express
 import express, { Request, Response } from 'express'
+
+//IMport other packages
 import bodyParser from 'body-parser'
+
+//Import routes
 import usersRoute from './routes/usersRoutes'
+import productsRoutes from './routes/productsRoute'
 
 const app: express.Application = express()
 const port: string = "3000"
@@ -13,6 +19,7 @@ app.get('/', function (req: Request, res: Response) {
 
 // call all the routes
 usersRoute(app);
+productsRoutes(app);
 
 app.listen(3000, function () {
     console.log(`🚀🚀 Storefont server started on port ${port}...`);
