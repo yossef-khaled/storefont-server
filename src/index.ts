c4e073c55df6cad09786a0ca606d6d51330c4518
yossef-khaled <yossef.k.y333@gmail.com>
2022-09-04 16:06:46 +0200

    Add orders routes to index file

--- src/index.ts
+++ src/index.ts
@@ -6,20 +6,22 @@ import bodyParser from 'body-parser'
 
 //Import routes
 import usersRoute from './routes/usersRoutes'
-import productsRoutes from './routes/productsRoute'
+import productsRoutes from './routes/productsRoutes'
+import ordersRoutes from './routes/ordersRoutes'
 
 const app: express.Application = express()
 const port: string = "3000"
 
 app.use(bodyParser.json())
 
-app.get('/', function (req: Request, res: Response) {
+app.get('/', function (_: Request, res: Response) {
     res.send('Hello World!')
 })
 
 // call all the routes
 usersRoute(app);
 productsRoutes(app);
+ordersRoutes(app);
 
 app.listen(3000, function () {
     console.log(`🚀🚀 Storefont server started on port ${port}...`);
