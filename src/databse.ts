0449438a52679e84755947f69766913d4b80ae28
yossef-khaled <yossef.k.y333@gmail.com>
2022-09-06 14:31:47 +0200

    Change DB when testing

--- src/databse.ts
+++ src/databse.ts
@@ -3,16 +3,20 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-const  {
+let  {
     POSTGRES_HOST,
     POSTGRES_DB,
+    POSTGRES_DB_TEST,
     POSTGRES_USER,
     POSTGRES_PASSWORD,
+    ENV
 } = process.env;
+
+console.log('ENV is : ', ENV);
 
 const client = new Pool({
     host: POSTGRES_HOST,
-    database: POSTGRES_DB,
+    database: ENV === 'dev' ? POSTGRES_DB : POSTGRES_DB_TEST,
     user: POSTGRES_USER,
     password: POSTGRES_PASSWORD,
 })
