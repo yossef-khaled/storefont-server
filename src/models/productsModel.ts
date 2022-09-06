7dfa5c1e92eda20f0f615af4c3220c458e1bf957
yossef-khaled <yossef.k.y333@gmail.com>
2022-09-06 14:33:18 +0200

    Add an add to cart method to products model

--- src/models/productsModel.ts
+++ src/models/productsModel.ts
@@ -1,6 +1,17 @@
 import client from "../databse";
 import Product from "../entities/product";
 
+export type TopProduct = {
+    productId: number;
+    timesOrdered: number;
+}
+
+export type CartProduct = {
+    productId: number;
+    orderId: number;
+    quantity: number;
+}
+
 class ProductsModel {
     async index(): Promise<Product[]> {
         try {
@@ -56,6 +67,25 @@ class ProductsModel {
         }
     }
 
+    async addToCart(orderId: number, productId: number, quantity: number): Promise<CartProduct> {
+        try {
+            const connection = await client.connect();
+
+            const data = await connection.query(`
+            INSERT INTO order_product ("orderId", "productId", quantity)
+            VALUES ($1, $2, $3) 
+            RETURNING *
+            ;
+            `, [orderId, productId, quantity]);
+
+            return data.rows[0];
+
+        }
+        catch (err) {
+            throw new Error(err.message);
+        }
+    }
+
     async productsByCategory(category: string): Promise<Product[]> {
         try {
             const connection = await client.connect();
@@ -74,19 +104,19 @@ class ProductsModel {
         }
     }
 
-    async topProducts(): Promise<Product[]> {
+    async topProducts(limit: number): Promise<TopProduct[]> {
         try {
             const connection = await client.connect();
             
             const data = await connection.query(`
-            SELECT "productId", COUNT("productId") AS times_ordered
+            SELECT "productId", COUNT("productId")::INT AS "timesOrdered"
             FROM order_product
             GROUP BY "productId"
-            ORDER BY times_ordered DESC
-            LIMIT 5
+            ORDER BY "timesOrdered" DESC
+            LIMIT $1
             ;
-            `);
-            
+            `, [limit]);
+
             connection.release();
             
             return data.rows;
