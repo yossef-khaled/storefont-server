ab31bc488a63b3dd27cd2786780f0145df145d21
yossef-khaled <yossef.k.y333@gmail.com>
2022-09-06 14:32:13 +0200

    Add a create method to orders model

--- src/models/ordersModel.ts
+++ src/models/ordersModel.ts
@@ -2,13 +2,31 @@ import Order from "../entities/order";
 import client from "../databse";
 
 class OrdersModel {
+    
+    async create(userId: number): Promise<Order> {
+        try {
+            const connection = await client.connect();
+            const data = await connection.query(`
+            INSERT INTO orders ("orderStatus", "userId")
+            VALUES('inCart', $1)
+            RETURNING *
+            ;`, [userId]);
+
+            return data.rows[0];
+        }
+        catch (err) {
+            throw new Error(err.message);
+        }
+    }
+
     async ordersHistory(id: number): Promise<Order[]> {
         try {
             const connection = await client.connect();
         
             const data = await connection.query(`
             SELECT * FROM orders WHERE orders."userId" = $1 
-            AND orders."orderStatus" = 'complete';
+            AND orders."orderStatus" = 'complete'
+            ;
             `
             , [id]);
             
@@ -22,14 +40,35 @@ class OrdersModel {
         }
     }
 
-    async currentOrder(id: number): Promise<Order> {
+    async show(userId: number): Promise<Order> {
         try {
             const connection = await client.connect();
         
             const data = await connection.query(`
             SELECT * FROM orders WHERE orders."userId" = $1 
-            AND orders."orderStatus" = 'inCart';
-            `, [id]);
+            AND orders."orderStatus" = 'inCart'
+            ;
+            `, [userId]);
+            
+            connection.release();
+            
+            return data.rows[0];
+        }
+        catch (err) {
+            console.log(err);
+            throw new Error(err); 
+        }
+    }
+
+    async currentOrder(userId: number): Promise<Order> {
+        try {
+            const connection = await client.connect();
+        
+            const data = await connection.query(`
+            SELECT * FROM orders WHERE orders."userId" = $1 
+            AND orders."orderStatus" = 'inCart'
+            ;
+            `, [userId]);
             
             connection.release();
             
